e75abe49a49a94066b67b01e84c8d0353d1e49eb
ardittristan <ardittristan@gmail.com>
2020-10-26 16:26:05 +0100

    trigger workflow

--- buttonOverflow.js
+++ buttonOverflow.js
@@ -1,9 +1,7 @@
 Hooks.on(
   "renderPlayerList",
-  /**
-   * @param {PlayerList} _playerList
-   * @param {JQuery} html
-   */
+  /**@param {PlayerList} _playerList
+   * @param {JQuery} html */
   (_playerList, html) => {
     document.body.style.setProperty("--playerlist-height", `${html.height()}px`);
   }
