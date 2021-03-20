adaf7b9b1f52dbb74f11c700d05f09a781009600
ardittristan <ardittristan@gmail.com>
2021-03-20 15:00:41 +0100

    compat

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,13 @@
 # Patch Notes
 
+## Version 1.0.0
+
+* 0.8.0 compatible.
+
+## Version 0.0.2
+
+* Remove unneeded code.
+
 ## Version 0.0.1
 
 * Initial release.
--- buttonOverflow.js
+++ buttonOverflow.js
@@ -1,8 +1,19 @@
-Hooks.on(
-  "renderPlayerList",
-  /**@param {PlayerList} _playerList
-   * @param {JQuery} html */
-  (_playerList, html) => {
-    document.body.style.setProperty("--playerlist-height", `${html.height()}px`);
-  }
-);
+window.buttonOverflow = {};
+
+Hooks.on("renderPlayerList", (_playerList, html) => {
+  document.body.style.setProperty("--playerlist-height", `${html.height()}px`);
+  calcOffset();
+});
+Hooks.on("renderSceneControls", (_sceneControls, html, options) => {
+  window.buttonOverflow.controlAmount = options.controls.length;
+  calcOffset();
+});
+window.addEventListener("resize", () => {
+  calcOffset();
+});
+
+function calcOffset() {
+  const controlsHeight = $("#controls").height();
+  const n = Math.ceil((window.buttonOverflow.controlAmount / controlsHeight) * 46) * 46 + 10;
+  document.body.style.setProperty("--playerlist-offset", `${n}px`);
+}
