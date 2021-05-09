f8fd02546271db4abaf2c9604a71439d33318fe1
ardittristan <ardittristan@gmail.com>
2021-05-09 17:56:34 +0200

    update on settings change
    closes #4

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,25 +1,29 @@
 # Patch Notes
 
+## Version 0.0.7
+
+- Recalc offset on setting change.
+
 ## Version 0.0.6
 
-* Add settings.
+- Add settings.
 
 ## Version 0.0.5
 
-* Fix to more consistent version of counting button amount.
+- Fix to more consistent version of counting button amount.
 
 ## Version 0.0.4
 
-* Add compat for new pf2e theme.
+- Add compat for new pf2e theme.
 
 ## Version 0.0.3
 
-* 0.8.0 compatible.
+- 0.8.0 compatible.
 
 ## Version 0.0.2
 
-* Remove unneeded code.
+- Remove unneeded code.
 
 ## Version 0.0.1
 
-* Initial release.
+- Initial release.
--- buttonOverflow.js
+++ buttonOverflow.js
@@ -1,8 +1,7 @@
 window.buttonOverflow = {};
 
 Hooks.on("renderPlayerList", (_playerList, html) => {
-  document.body.style.setProperty("--playerlist-height", `${html.height() + (window.buttonOverflow.playerlistOffset || 0)}px`);
-  calcOffset();
+  calcPlayerListOffset();
 });
 
 window.addEventListener("resize", () => {
@@ -13,6 +12,11 @@ function calcOffset() {
   const controlsHeight = $("#controls").height();
   const n = Math.ceil(((window.buttonOverflow.controlAmount - (window.buttonOverflow.hiddenButtons || 0)) / controlsHeight) * 46) * 46 + 10;
   document.body.style.setProperty("--playerlist-offset", `${n}px`);
+}
+
+function calcPlayerListOffset() {
+  document.body.style.setProperty("--playerlist-height", `${html.height() + (window.buttonOverflow.playerlistOffset || 0)}px`);
+  calcOffset();
 }
 
 Hooks.on("ready", () => {
@@ -29,6 +33,7 @@ Hooks.on("init", () => {
     default: 0,
     onChange: (val) => {
       window.buttonOverflow.playerlistOffset = val;
+      calcPlayerListOffset();
     },
   });
   game.settings.register("buttonoverflow", "hiddenButtons", {
@@ -39,6 +44,7 @@ Hooks.on("init", () => {
     default: 0,
     onChange: (val) => {
       window.buttonOverflow.hiddenButtons = val;
+      calcOffset();
     },
   });
   window.buttonOverflow.playerlistOffset = game.settings.get("buttonoverflow", "playerlistOffset");
